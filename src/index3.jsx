9b5a2b898eee0c72e46d249c808116eaae11b6dd
emariif <okbro8788@gmail.com>
2024-08-20 22:59:30 +0700

    first commit

---
+++ src/index3.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import CounterApp from "./CounterApp";
+
+const root = createRoot(document.getElementById('root'));
+root.render(<CounterApp />);
